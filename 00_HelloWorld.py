ecdd7f8967fb3bbba475de74d457aeb7d2952829
Administrator <486980690@qq.com>
2025-11-29 13:12:00 +0800

    首次提交

---
+++ 00_HelloWorld.py
@@ -0,0 +1,39 @@
+print("1 World")
+print("Hello World")
+print("2 Worl")
+'''hello'''
+'''1、字面量'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_变量、type.py
@@ -0,0 +1,22 @@
+# 变量名称 = 变量的值
+# 变量
+
+a = 1
+b = 3
+print(a)
+print(b)
+print(a+b)
+money = 20
+
+
+a = "123"
+
+print(money)
+print(a,b,"nh")
+print(a,b,"nh")
+
+print(type(a))
+print(type("123"))
+print(type(1))
+print(type(1.1))
+
